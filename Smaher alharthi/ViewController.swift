22b21667fec742783cb580c61a8aa39d499358f4
smaher <smaher.1416.it@gmail.com>
2021-12-09 13:19:30 +0300

    Initial Commit

---
+++ Smaher alharthi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Smaher alharthi
+//
+//  Created by smaher on 05/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
